9715cc5978b1f1212e60fe65471ef2a99e0eb247
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-26 15:52:20 -0400

    added code to photolist

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 
-export default function PhotoListItem() {
-  return (
-    <article className="photo-list__item">
-      <img 
-    className="photo-list__image"
-        src="https://images.unsplash.com/photo-1636489950038-4064f1eead72"
-      alt="Sample"
-      />
-      <div className="photo-list__user-details">
-        <img 
-        className="photo-list__user-profile"
-          src="https://www.gravatar.com/avatar"
-          alt="User"
-        />
-        <div className="photo-list__user-info">
-          <span className="photo-list__user-name">Joe Example</span>
-          <span className="photo-list__user-location">Toronto, Canada</span>
-        </div>
-      </div>
-    </article>
-  );
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+
+const PhotoListItem = () => {
+
 }
