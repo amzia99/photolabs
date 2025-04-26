eea54d4f561a6e68d5ea1a96e91dc65f4677a64e
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-26 16:35:44 -0400

    linked pictures into app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+// Add sample data
+const samplePhoto = {
+  imageSource: "/image-1-Full.jpeg",
+  username: "John Smith",
+  profile: "/profile-1.jpg",
+  location: {
+    city: "Toronto",
+    country: "Canada"
+  }
+};
 
 const App = () => (
   <div className="App">
-    <PhotoListItem/>
+    <PhotoListItem 
+      imageSource={samplePhoto.imageSource}
+      username={samplePhoto.username}
+      profile={samplePhoto.profile}
+      location={samplePhoto.location}
+    />
   </div>
-)
+);
 
-export default App
+export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-export default function PhotoListItem() {
+export default function PhotoListItem({ imageSource, username, profile, location }) {
   return (
     <article className="photo-list__item">
       <img 
         className="photo-list__image"
-        src={sampleDataForPhotoListItem.imageSource}
-        alt="Sample"
+        src={imageSource}
+        alt={`Photo by ${username}`}
       />
       <div className="photo-list__user-details">
         <img 
           className="photo-list__user-profile"
-          src={sampleDataForPhotoListItem.profile}
-          alt="User"
+          src={profile}
+          alt={username}
         />
         <div className="photo-list__user-info">
-          <span className="photo-list__user-name">{sampleDataForPhotoListItem.username}</span>
+          <span className="photo-list__user-name">{username}</span>
           <span className="photo-list__user-location">
-            {sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}
+            {location.city}, {location.country}
           </span>
         </div>
       </div>
