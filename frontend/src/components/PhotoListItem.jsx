c55faab4facb10fef9cd78b947be061dcded0187
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-26 16:03:38 -0400

    added code to render photos to phottolist item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -11,7 +11,27 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-
-const PhotoListItem = () => {
-
+export default function PhotoListItem() {
+  return (
+    <article className="photo-list__item">
+      <img 
+        className="photo-list__image"
+        src={sampleDataForPhotoListItem.imageSource}
+        alt="Sample"
+      />
+      <div className="photo-list__user-details">
+        <img 
+          className="photo-list__user-profile"
+          src={sampleDataForPhotoListItem.profile}
+          alt="User"
+        />
+        <div className="photo-list__user-info">
+          <span className="photo-list__user-name">{sampleDataForPhotoListItem.username}</span>
+          <span className="photo-list__user-location">
+            {sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}
+          </span>
+        </div>
+      </div>
+    </article>
+  );
 }
