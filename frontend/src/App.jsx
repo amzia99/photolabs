c0dd9aedf4ba8f8fb6b5d4ea354d16d9f423bd2a
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-26 21:20:02 -0400

    fixed app.jsx for sample data for 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,48 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-// Add sample data
-const samplePhoto = {
-  imageSource: "/Image-1-Full.jpeg",
-  username: "John Example",
-  profile: "/profile-1.jpg",
-  location: {
-    city: "Montreal",
-    country: "Canada"
+// Sample photo data for 3 phootos
+const samplePhotos = [
+  {
+    id: 1,
+    imageSource: "/Image-1-Full.jpeg",
+    username: "John Example",
+    profile: "/profile-1.jpg",
+    location: {
+      city: "Montreal",
+      country: "Canada"
+    }
+  },
+  {
+    id: 2,
+    imageSource: "/Image-2-Full.jpeg",
+    username: "Jane Doe",
+    profile: "/profile-1.jpg",
+    location: {
+      city: "Toronto",
+      country: "Canada"
+    }
+  },
+  {
+    id: 3,
+    imageSource: "/Image-3-Full.jpeg",
+    username: "Alex Smith",
+    profile: "/profile-1.jpg",
+    location: {
+      city: "Vancouver",
+      country: "Canada"
+    }
   }
-};
+];
 
 const App = () => (
   <div className="App">
-    <PhotoListItem photo={samplePhoto} />
+    {samplePhotos.map(photo => (
+      <PhotoListItem 
+        key={photo.id}
+        photo={photo}
+      />
+    ))}
   </div>
 );
 
