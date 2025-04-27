bcd2ccc2d50bbde6661447bc7b8dd5aca2d9ce6f
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-26 23:04:25 -0400

    Modified topnavigationbar.jsx for topics and icon

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,20 @@
-import '../styles/TopNavigationBar.scss'
+import React from 'react';
+import '../styles/TopNavigationBar.scss';
+import TopicList from './TopicList';
+import FavIcon from './FavIcon';
 
-const TopNavigation = () => {
+const TopNavigationBar = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <div className="top-nav-bar__content">
+        <TopicList />
+        <div className="top-nav-bar__favorites">
+          <FavIcon />
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
-export default TopNavigation;
+export default TopNavigationBar;
