0a9e604e969df7414d1419bfae65947dea57c567
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-26 22:49:03 -0400

    added code to topiclistitem.jsx

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,15 +1,10 @@
-import "../styles/TopicListItem.scss";
+import React from 'react';
+import '../styles/TopicListItem.scss';
 
-const sampleDataForTopicListItem = {
-  id: 1,
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
+const TopicListItem = ({ label }) => {
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span>{label}</span>
     </div>
   );
 };
