2f9cc6938f33b6e7847cb33bf674e56900f0c710
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-28 01:33:10 -0400

    added notification to photos in favbadge

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,13 +1,12 @@
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon selected={!!isFavPhotoExist}/>
+      <FavIcon displayAlert={isFavPhotoExist} selected={true} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import '../styles/TopNavigationBar.scss';
 import TopicList from './TopicList';
-import FavIcon from './FavIcon';
+import FavBadge from './FavBadge';
 
-const TopNavigationBar = () => {
+const TopNavigationBar = ({ favouritePhotos }) => {
+  const isFavPhotoExist = favouritePhotos && favouritePhotos.length > 0;
+  
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <div className="top-nav-bar__content">
         <TopicList />
         <div className="top-nav-bar__favorites">
-          <FavIcon />
+          <FavBadge isFavPhotoExist={isFavPhotoExist} />
         </div>
       </div>
     </div>
