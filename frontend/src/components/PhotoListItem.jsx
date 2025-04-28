a6b3dafdbb293c5464f515d99960d8380ec8e73a
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-28 00:44:11 -0400

    updated photolistitem to handle mock data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,20 +3,29 @@ import PhotoFavButton from './PhotoFavButton';
 import '../styles/PhotoListItem.scss';
 
 export default function PhotoListItem({ photo }) {
+  
   const {
-    imageSource = '/placeholder-image.jpg',
-    username = 'Anonymous',
-    profile = '/default-profile.jpg',
-    location = { city: 'Unknown', country: 'Unknown' }
+    imageSource,
+    username,
+    profile,
+    location,
+    urls,
+    user
   } = photo || {};
+
+
+  const imgSrc = imageSource || (urls && urls.full);
+  const userName = username || (user && user.name);
+  const userProfile = profile || (user && user.profile);
+  const userLocation = location || {};
 
   return (
     <article className="photo-list__item">
       <div className="photo-list__image-container">
         <img 
           className="photo-list__image"
-          src={imageSource}
-          alt={`Photo by ${username}`}
+          src={imgSrc}
+          alt={`Photo by ${userName}`}
         />
         <div className="photo-list__fav-button">
           <PhotoFavButton photo={photo} />
@@ -25,13 +34,13 @@ export default function PhotoListItem({ photo }) {
       <div className="photo-list__user-details">
         <img 
           className="photo-list__user-profile"
-          src={profile}
-          alt={username}
+          src={userProfile}
+          alt={userName}
         />
         <div className="photo-list__user-info">
-          <span className="photo-list__user-name">{username}</span>
+          <span className="photo-list__user-name">{userName}</span>
           <span className="photo-list__user-location">
-            {location.city}, {location.country}
+            {userLocation.city}, {userLocation.country}
           </span>
         </div>
       </div>
