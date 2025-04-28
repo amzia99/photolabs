b57ec5d8ccce32c8060f73c9d9ed8b1cdb09cffd
Abdullah Zia <amz1cflp99@gmail.com>
2025-04-28 00:26:49 -0400

    added homeroute.jsx to routes

---
+++ frontend/src/routes/HomeRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopNavigationBar from './TopNavigationBar';
+import PhotoList from './PhotoList';
+import '../styles/HomeRoute.scss';
+
+const HomeRoute = (props) => {
+  return (
+    <div className="home-route">
+      <TopNavigationBar />
+      <PhotoList photos={props.photos} />
+    </div>
+  );
+};
+
+export default HomeRoute;
